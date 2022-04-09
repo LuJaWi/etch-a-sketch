40360fbc677632adca87ac9e95b7fc36a8688606
Luke Witt <Witt.Luke@gmail.com>
2022-04-09 11:15:55 -0400

    Page formatting updates

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ console.log(easSelector);
 
 let childPixelDiv = 'div'
 
-let gridEdgeLength = 4;
-let easPixelEdgeLength = String(1024/gridEdgeLength + 'px');
+let pixelEdgeCount = 5; // Number of etch-a-sketch pixels per edge
+let easPixelEdgeLength = String(50/pixelEdgeCount + 'vh'); // heigth/width of each pixel converted to css style
 
 console.log(easPixelEdgeLength);
 
 
 
-for (i = gridEdgeLength**2; i>0; i--) {
+for (i = pixelEdgeCount**2; i>0; i--) {
     easSelector.innerHTML += "<div class='eas-pixel'>test</div>"
 }
 
@@ -24,4 +24,5 @@ const easPixels = document.querySelectorAll(".eas-pixel");
 easPixels.forEach(pixel => {
     pixel.style.height = easPixelEdgeLength;
     pixel.style.width = easPixelEdgeLength;
+    pixel.style.border = 'solid 1px'
 });
