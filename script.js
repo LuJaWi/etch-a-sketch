c773bbe1d17db201e7265510c1a0cf95acf4efc2
Luke Witt <Witt.Luke@gmail.com>
2022-04-09 09:46:53 -0400

    Added pixel calculating script

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const easSelector = document.querySelector(".etch-a-sketch");
-console.log(easSelector)
+
+
+
+console.log(easSelector);
+
 
 
 let childPixelDiv = 'div'
 
-gridEdge = 5
+let gridEdgeLength = 4;
+let easPixelEdgeLength = String(1024/gridEdgeLength + 'px');
 
-for (i = gridEdge**2; i>0; i--) {
+console.log(easPixelEdgeLength);
+
+
+
+for (i = gridEdgeLength**2; i>0; i--) {
     easSelector.innerHTML += "<div class='eas-pixel'>test</div>"
 }
+
+const easPixels = document.querySelectorAll(".eas-pixel");
+
+easPixels.forEach(pixel => {
+    pixel.style.height = easPixelEdgeLength;
+    pixel.style.width = easPixelEdgeLength;
+});
