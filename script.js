c35be469ce151f50d374f7a28d6a3cebc587ecf5
Luke Witt <Witt.Luke@gmail.com>
2022-04-10 15:24:35 -0400

    Added eraser function. Started on slider for size

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const easSelector = document.querySelector(".etch-a-sketch");
 
-let pixelEdgeCount = 25; // Number of etch-a-sketch pixels per edge
+let pixelEdgeCount = document.getElementById('canvas-size').value; // Number of etch-a-sketch pixels per edge
 let easPixelEdgeLength = String(50/pixelEdgeCount + 'vh'); // heigth/width of each pixel converted to css style
 
 // Creates grid based on pixel edge count
 for (i = pixelEdgeCount**2; i>0; i--) {
     easSelector.innerHTML += '<div class="eas-pixel"></div>';
 };
+
+
 
 const easPixels = document.querySelectorAll(".eas-pixel"); // Query selector for all created pixels
 
@@ -28,6 +30,8 @@ easPixels.forEach(pixel => {
         pixel.style.backgroundColor = 'rgb(' + randomRGB() + ')';}
         if(document.getElementById('black').checked){
             pixel.style.backgroundColor = 'rgb(48,48,48)';}
+        if(document.getElementById('eraser').checked){
+            pixel.style.backgroundColor = 'rgb(255,255,255)';}
 
     });
     // resets all pixels when 'reset' button is clicked
