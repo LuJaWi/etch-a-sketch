d56b0107b77f8e11760581461c375d71e78fc712
Luke Witt <Witt.Luke@gmail.com>
2022-04-10 14:10:48 -0400

    Added option to choose between black and rainbow

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const easSelector = document.querySelector(".etch-a-sketch");
 
-let pixelEdgeCount = 20; // Number of etch-a-sketch pixels per edge
+let pixelEdgeCount = 25; // Number of etch-a-sketch pixels per edge
 let easPixelEdgeLength = String(50/pixelEdgeCount + 'vh'); // heigth/width of each pixel converted to css style
 
 // Creates grid based on pixel edge count
@@ -17,17 +17,29 @@ easPixels.forEach(pixel => {
     pixel.style.border = 'solid 1px';
 });
 
+// Selector variable for reset button
 const resetEAS = document.querySelector(".reset-button")
 
 // Add event listeners that can influence every pixel
 easPixels.forEach(pixel => {
     // adds class on mouseover (per pixel)
     pixel.addEventListener('mouseover', () => {
-        pixel.classList.add("grayfill");
+        if(document.getElementById('rainbow').checked){
+        pixel.style.backgroundColor = 'rgb(' + randomRGB() + ')';}
+        if(document.getElementById('black').checked){
+            pixel.style.backgroundColor = 'rgb(48,48,48)';}
+
     });
     // resets all pixels when 'reset' button is clicked
     resetEAS.addEventListener('click', () => {
-        pixel.classList.remove("grayfill");
+        pixel.style.backgroundColor = 'rgb(255,255,255)';
     }); 
 });
 
+const randomRGB = function(){
+    let red = Math.round(Math.random()*255);
+    let green = Math.round(Math.random()*255);
+    let blue = Math.round(Math.random()*255);
+    let rgb = String(red + ", " + green + ", " + blue)
+    return rgb;
+};
