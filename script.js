c228ffdc3cfa566968c7f3f0e32b7ec188092742
Luke Witt <Witt.Luke@gmail.com>
2022-04-10 08:14:34 -0400

    Basic etch-a-sketch function added

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 const easSelector = document.querySelector(".etch-a-sketch");
 
-
-
-console.log(easSelector);
-
-
-
-let childPixelDiv = 'div'
-
-let pixelEdgeCount = 5; // Number of etch-a-sketch pixels per edge
+let pixelEdgeCount = 20; // Number of etch-a-sketch pixels per edge
 let easPixelEdgeLength = String(50/pixelEdgeCount + 'vh'); // heigth/width of each pixel converted to css style
 
-console.log(easPixelEdgeLength);
-
-
-
+// Creates grid based on pixel edge count
 for (i = pixelEdgeCount**2; i>0; i--) {
-    easSelector.innerHTML += "<div class='eas-pixel'>test</div>"
-}
+    easSelector.innerHTML += "<div class='eas-pixel'></div>"
+};
 
-const easPixels = document.querySelectorAll(".eas-pixel");
+const easPixels = document.querySelectorAll(".eas-pixel"); // Query selector for all created pixels
 
+// Adds correct base formatting for all pixels
 easPixels.forEach(pixel => {
     pixel.style.height = easPixelEdgeLength;
     pixel.style.width = easPixelEdgeLength;
     pixel.style.border = 'solid 1px'
+});
+
+// Add event listener that adds a class on mouse over
+easPixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', () => {
+        pixel.classList.add('grayfill')
+    });
 });
